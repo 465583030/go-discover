5919e2126e0491423d529ca8cdc28993d3d1eafc
Donovan Keohane <pinoycoding@gmail.com>
2017-04-11 22:02:53 -0400

    save

--- remake/main.go
+++ remake/main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"io"
+	"log"
 	"net"
-	"sync"
+	"strings"
 )
 
-var wg sync.WaitGroup
+func inc(ip net.IP) {
+	for j := len(ip) - 1; j >= 0; j-- {
+		ip[j]++
+		if ip[j] > 0 {
+			break
+		}
+	}
+}
 
 func RequestHTTP(ip string, port string) (bool, string) {
 	conn, err := net.Dial("tcp", ip+":"+port)
@@ -24,7 +33,41 @@ func RequestHTTP(ip string, port string) (bool, string) {
 }
 
 func main() {
-	a, b := RequestHTTP("google.com", "80")
-	fmt.Println(a)
-	fmt.Println(b)
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		panic(err)
+	}
+	addr, err := func() (net.Addr, error) {
+		for _, iface := range ifaces {
+			addrs, ifaceErr := iface.Addrs()
+			if ifaceErr != nil {
+				log.Fatal(err)
+			}
+			for _, addr := range addrs {
+				oip, _, cidrErr := net.ParseCIDR(addr.String())
+				if cidrErr != nil {
+					log.Fatal(err)
+				}
+				if str := strings.HasPrefix(addr.String(), "127"); str == false {
+					if oip.To4() != nil {
+						return addr, nil
+					}
+				}
+			}
+		}
+		return nil, errors.New("Error occured in getting CIDR.")
+	}()
+
+	if err != nil {
+		panic(err)
+	}
+
+	oip, ipnet, err := net.ParseCIDR(addr.String())
+
+	for ip := oip.Mask(ipnet.Mask); ipnet.Contains(ip); inc(ip) {
+		a, b := RequestHTTP(ip.String(), "")
+		if a {
+			fmt.Println(b)
+		}
+	}
 }
